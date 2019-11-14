ec9e9f12ed6cedc4b294830c7a00d64109469604
este001 <estefan.rm2@gmail.com>
2019-11-14 12:23:19 +0100

    added server logic for newly connected clients

--- server.py
+++ server.py
@@ -1,5 +1,36 @@
 """Server for game/chat"""
 
+import socket
+import threading
+
+
+def username_exists(user_name):
+    if user_name in clients.values():
+        return True
+
+
+def client_connected(conn):
+    while True:
+        user_name = conn.recv(1024)
+
+        if not user_name:
+            break
+        if username_exists(user_name):
+            conn.sendall('0'.encode('utf-8'))
+
+
 if __name__ == '__main__':
-    """test branch"""
-    pass
+    IP = "127.0.0.1"
+    PORT = 1234
+
+    clients = {}
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((IP, PORT))
+    server_socket.listen()
+
+    while True:
+        conn, addr = server_socket.accept()
+
+        client_thread = threading.Thread(target=client_connected, args=(conn,))
+        client_thread.start()
