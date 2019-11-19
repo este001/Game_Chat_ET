73fe54560e1e03f71362253c92c2c912677e02cc
este001 <estefan.rm2@gmail.com>
2019-11-19 20:09:18 +0100

    started game logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,36 +1,60 @@
 """
 TIC TAC TOE
 Game logic
+ps = x
+['x', 'o', 'x']   = ['ps', 'ps', 'ps']
 """
 
 
-def horizontal_win_condition(board):
-    if board[0][0:4] == ['x', 'x', 'x']:
+def horizontal_win_condition(board, player_symbol):
+    if board[0][0:4] == [player_symbol, player_symbol, player_symbol]:
         return True
-    elif board[1][0:4] == ['x', 'x', 'x']:
+    elif board[1][0:4] == [player_symbol, player_symbol, player_symbol]:
         return True
-    elif board[2][0:4] == ['x', 'x', 'x']:
+    elif board[2][0:4] == [player_symbol, player_symbol, player_symbol]:
         return True
     else:
         return False
 
 
-def vertical_win_condition():
-    pass
+def vertical_win_condition(board, player_symbol):
+    if board[0][0] == player_symbol and board[1][0] == player_symbol and board[2][0] == player_symbol:
+        return True
+    elif board[0][1] == player_symbol and board[1][1] == player_symbol and board[2][1] == player_symbol:
+        return True
+    elif board[0][2] == player_symbol and board[1][2] == player_symbol and board[2][2] == player_symbol:
+        return True
+    else:
+        return False
 
 
-def diagonal_win_condition():
-    pass
+def diagonal_win_condition(board, player_symbol):
+    if board[0][0] == player_symbol and board[1][1] == player_symbol and board[2][2] == player_symbol:
+        return True
+    elif board[0][2] == player_symbol and board[1][1] == player_symbol and board[2][0] == player_symbol:
+        return True
+    else:
+        return False
 
 
-def check_win_condition():
-    # if horizontal() or vertical() or diagonal():
-    pass
+def tie(board):
+    for row in board:
+        for i in row:
+            if i == '-':
+                return False
+    return True
 
 
-def user_input(coordinate, board, player_input):
+def check_win_condition(board, player_symbol):
+    if horizontal_win_condition(board, player_symbol) or vertical_win_condition(board, player_symbol) or diagonal_win_condition(board, player_symbol):
+        return True
+    elif tie(board):
+        return True
+
+
+def user_input(coordinate, board, player_symbol):
     board[coordinate[0]].pop(coordinate[1])
-    board[coordinate[0]].insert(coordinate[1], player_input)
+    board[coordinate[0]].insert(coordinate[1], player_symbol)
     return board
 
 
