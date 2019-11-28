03b1a3e3d5a75218d54ba6efd0c7de7540da2f37
este001 <estefan.rm2@gmail.com>
2019-11-28 20:47:26 +0100

    Major server refactor

--- server.py
+++ server.py
@@ -3,12 +3,35 @@ import threading
 import time
 
 
-def send_message(message, clients):
+def strip_header(message):
+    """Returns the message striped of it's header"""
+
+    message = message[1:]
+    return message
+
+
+def whisper_message(whispered_message, clients, conn):
+    """Sends direct message to all users with an @ sign"""
+
+    whispered_message = strip_header(whispered_message)
+    user_to_whisper_list = whispered_message.split()
+    user_to_whisper_list = [user.strip('@') for user in user_to_whisper_list if user[0] == '@']
     for c in clients:
-        c.sendall(message.encode('utf'))
+        if [True for user in user_to_whisper_list if clients[c] == user]:
+            c.sendall(f'S{clients[conn]} whispered > {whispered_message}'.encode('utf-8'))
+    conn.sendall(f'S{clients[conn]} whispered > {whispered_message}'.encode('utf-8'))
+
+
+def send_message(message, clients):
+    """Sends message to all users ex. Tim has connected and sends list of users online"""
+
+    for c in clients:
+        c.sendall(message.encode('utf-8'))
 
 
 def username_exists(user_name, clients):
+    """Checks if username exists in clients dict"""
+
     if user_name in clients.values():
         return True
     else:
@@ -16,7 +39,8 @@ def username_exists(user_name, clients):
 
 
 def broadcast_message(message, clients, conn):
-    message = message.decode('utf-8')
+    """ Broadcasts message to all users """
+
     user = clients[conn]
     message = f"S{user} > {message[1:]}".encode('utf-8')
     for c in clients:
@@ -24,15 +48,17 @@ def broadcast_message(message, clients, conn):
 
 
 def player_accepted_challenge(clients, player1, player2):
-    player1 = player1.decode('utf-8')
+    """ Returns an accept message to the challenger """
+
     for c in clients:
         if clients[c] == player1:
             c.sendall(f"A{clients[c]} has accepted {clients[player2]} challenge".encode('utf-8'))
 
 
 def player_declined_challenge(clients, name, conn):
-    challenger = name[1:]
-    challenger = challenger.decode('utf-8')
+    """ Returns a decline message to the challenger"""
+
+    challenger = strip_header(name)
     player_game_status[clients[conn]] = True
     player_game_status[challenger] = True
     message = f"D{clients[conn]} has declined a challenge from {challenger}"
@@ -43,11 +69,15 @@ def player_declined_challenge(clients, name, conn):
 
 
 def reset_player_availability(conn, clients):
+    """ Resets user availability """
+
     player_game_status[clients[conn]] = True
     return True
 
 
 def player_availability(player):
+    """ Returns true or false if player is available"""
+
     if player_game_status[player]:
         return True
     else:
@@ -55,10 +85,11 @@ def player_availability(player):
 
 
 def game_challenge(conn, message, clients):
-    message = message.decode('utf-8')
+    """ Sends a challenge message to a user """
+
     challenger = clients[conn]
     player_game_status[clients[conn]] = False
-    player_to_challenge = message[1:]
+    player_to_challenge = strip_header(message)
 
     player_game_status[player_to_challenge] = False
     for c in clients:
@@ -66,52 +97,54 @@ def game_challenge(conn, message, clients):
             c.sendall(f'C{challenger}'.encode('utf-8'))
 
 
-def whisper_message(whispered_message, clients, conn):
-    whispered_message = whispered_message[1:]
-    user_to_whisper_list = whispered_message.split()
-    user_to_whisper_list = [user.strip('@') for user in user_to_whisper_list if user[0] == '@']
+def game_turn_coordinates(message):
+    """ Sends coordinats to players """
+
+    receiver = message[3:]
+    game_move = message[1:3]
     for c in clients:
-        if [True for user in user_to_whisper_list if clients[c] == user]:
-            c.sendall(f'S{clients[conn]} whispered > {whispered_message}'.encode('utf-8'))
-    conn.sendall(f'S{clients[conn]} whispered > {whispered_message}'.encode('utf-8'))
+        if clients[c] == receiver:
+            message = f"G{game_move}{clients[conn]}"
+            c.sendall(message.encode('utf-8'))
 
 
 def receive_messages(conn):
     try:
         while True:
             message = conn.recv(1024)
-            if message.decode('utf-8')[0] == "Q":
+            message = message.decode('utf-8')
+            # Quit header
+            if message[0] == "Q":
                 break
+            # Whisper header
+            elif '@' in message:
+                whisper_message(message, clients, conn)
 
-            elif '@' in message.decode('utf-8'):
-                whisper_message(message.decode('utf-8'), clients, conn)
-
-            elif message[0:1].decode('utf-8') == "S":
+            # Broadcast header
+            elif message[0:1] == "S":
                 broadcast_message(message, clients, conn)
 
-            elif message[0:1].decode('utf-8') == "C":
-                if player_availability(message[1:].decode('utf-8')):
+            # Challenge header
+            elif message[0:1] == "C":
+                if player_availability(message[1:]):
                     game_challenge(conn, message, clients)
                 else:
                     conn.sendall(f'DPlayer is unavailable'.encode('utf-8'))
-            elif message[0:1].decode('utf-8') == "A":
 
+            # Accept challenge header
+            elif message[0:1] == "A":
                 player_accepted_challenge(clients, message[1:], conn)
 
-            elif message[0:1].decode('utf-8') == "G":
+            # Game coordiantes header
+            elif message[0:1] == "G":
+                game_turn_coordinates(message)
 
-                receiver = message[3:].decode('utf-8')
-                game_move = message[1:3].decode('utf-8')
-                for c in clients:
-                    if clients[c] == receiver:
-                        message = f"G{game_move}{clients[conn]}"
-                        c.sendall(message.encode('utf-8'))
-
-            elif message[0:1].decode('utf-8') == "D":
-
+            # Decline header
+            elif message[0:1] == "D":
                 player_declined_challenge(clients, message, conn)
 
-            elif message[0:1].decode('utf-8') == "R":
+            # Reset player status header
+            elif message[0:1] == "R":
                 reset_player_availability(conn, clients)
 
         print(f"{clients[conn]} has disconnected")
@@ -139,15 +172,19 @@ def client_connected(conn):
     try:
         while True:
             user_name = conn.recv(1024)
-            if user_name.decode('utf-8')[0] == "Q":
+            user_name = strip_header(user_name.decode('utf-8'))
+
+            if user_name[0] == "Q":
                 break
-            if username_exists(user_name.decode('utf-8')[1:], clients):
+
+            if username_exists(user_name[1:], clients):
                 conn.sendall('1'.encode('utf-8'))
+
             else:
                 conn.sendall('0'.encode('utf-8'))
-                clients[conn] = user_name[1:].decode('utf-8')
-                player_game_status[user_name[1:].decode('utf-8')] = True
-                broadcast_message(message="Shas connected".encode('utf-8'), conn=conn, clients=clients)
+                clients[conn] = user_name
+                player_game_status[user_name] = True
+                broadcast_message(message="Shas connected", conn=conn, clients=clients)
                 time.sleep(1)
                 send_message(online_users(clients), clients)
                 receive_messages(conn)
